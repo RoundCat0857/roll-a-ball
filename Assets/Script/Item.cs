466008695c01660a25de4064604f871923123e16
Round Cat <round0857cat@gmail.com>
2019-02-06 14:30:54 +0900

    add Destroy

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Item : MonoBehaviour{
+  // トリガーとの接触時に呼ばれるコールバック
+  void OnTriggerEnter (Collider hit) {
+    // 接触対象はPlayerタグですか？
+    if (hit.CompareTag ("Player")) {
+      Destroy(gameObject);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 }
