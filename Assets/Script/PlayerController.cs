d9fa4d90a4a5ae5a7b49d5b8f17434b4601956bb
Mao0857 <yogh0857mao@gmail.com>
2019-02-06 13:55:03 +0900

    first commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  public float speed = 10;
+  void FixedUpdate() {
+    // 入力を変数に格納
+    float x = Input.GetAxis("Horizontal");
+    float z = Input.GetAxis("Vertical");
+    // Rigidbodyコンポーネントの取得
+    Rigidbody rigidbody = GetComponent<Rigidbody>();
+    // 入力をrigidbodyに反映
+    rigidbody.AddForce(x, 0, z);
+  }
+}
